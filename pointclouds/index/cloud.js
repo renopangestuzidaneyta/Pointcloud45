1e1b93c49e57a5956b3443c1bf5643f8ee6e7563
renopangestuzidaneyta <renopangestuzidane1234@gmail.com>
2026-09-18 20:12:40 +0700

    add point clouds to web files

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 7597898,
+    "boundingBox": {
+        "lx": 526412.8263549805,
+        "ly": 9406805.7789917,
+        "lz": 131.9271240234375,
+        "ux": 526478.176361084,
+        "uy": 9406871.128997803,
+        "uz": 197.27713012695313
+    },
+    "tightBoundingBox": {
+        "lx": 526412.8263549805,
+        "ly": 9406805.7789917,
+        "lz": 131.9271240234375,
+        "ux": 526478.1763549805,
+        "uy": 9406870.1889917,
+        "uz": 150.5071240234375
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.5659476518630981,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
